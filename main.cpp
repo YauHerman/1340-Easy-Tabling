35b08f0f076030fb1e1939815877a9210a6732be
Yau Herman <taikongrenqiu@hotmail.com>
2019-04-25 20:07:15 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,12 @@
 #include <string>
 #include <fstream>
 #include <tablemanager>
+#include "Table.h"
+#include "TableManager.h"
 using namespace std;
 
-
-
 int main(){
+  Table table = new Table;
+  table.table_num = 999;
+  cout << testFunction(table) << endl;
 }
