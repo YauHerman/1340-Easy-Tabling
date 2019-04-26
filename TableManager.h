2c8d74984305d58211b3ed12ac71a03554cd4ed1
Toming <47220972+HKU3556759@users.noreply.github.com>
2019-04-26 14:27:24 +0800

    Update TableManager.h

--- TableManager.h
+++ TableManager.h
@@ -8,12 +8,27 @@ using namespace std;
 vector<Table> tables;
 
 //TODO Change table configuration
+void change_table_config( string order, table *head, int target_table, int no_of_customers){
+  // this first read an order ("add"-add a table , "delete"-delete a table, "change" - change no of customers)
+  //this function then reads a pointer pointing to the head of linked list, table no. of targeted table and new no. of customers.
+if (order == "add"){
+  for (int i=0; i< target_table
+}
 
 //TODO Reserve a table
-
+void reserve_table (table *head, int target_table){
+  //this function then reads a pointer pointing to the head of linked list, table no. of targeted table
+  
+}
 //TODO Occupy a table
-
-//TODO Release a table
+void occupy_table (table *head, int target_table){
+  //this function then reads a pointer pointing to the head of linked list, table no. of targeted table, 
+}
+  
+  //TODO Release a table
+void release_table (table *head, int target_table){
+  //this function then reads a pointer pointing to the head of linked list, table no. of targeted table
+}
 
 void addTable(Table t) {
   tables.push_back(t);
