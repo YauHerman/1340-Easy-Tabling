67740865d3b46f02dd178ee073c1eea27acc9bb8
Toming <47220972+HKU3556759@users.noreply.github.com>
2019-04-26 16:55:42 +0800

    Update TableManager.h

--- TableManager.h
+++ TableManager.h
@@ -7,17 +7,35 @@ using namespace std;
 
 vector<Table> tables;
 
-//search for a table given the table no.
-void searchtable(int table_no, table *head){
+//search for a table given a table no(key).
+int search_if_table_exist(int key, table *head){
   table *current = head;
-  while (current->next)  
+  while (current != NULL) {
+    // Idk is this while loop legal lol
+    if (current -> table_no == key){
+      return 1;}
+    current = current->next}
+  return 0;}
+  
+  
 
 //TODO Change table configuration
-void change_table_config( string order, table *&head, int target_table, int no_of_customers){
+void change_table_config( string order, table *&head, int table_no, int no_of_cus){
   // this first read an order ("add"-add a table , "delete"-delete a table, "change" - change no of customers)
   //this function then reads a pointer pointing to the head of linked list, table no. of targeted table and new no. of customers.
 if (order == "add"){
-  current = 
+  if(search_if_table_exist(table_no, head)){
+    cout << "Table already existed" << endl;}
+  else{
+  table *p = new table;
+  p->table-no = table_no;
+  p->no_of_cus = no_of_cus;
+  p->status = NULL;
+  p->amount = NULL;
+  p->time = NULL;
+  p->next = head;
+  head = p;}
+  }
   
 }
 
