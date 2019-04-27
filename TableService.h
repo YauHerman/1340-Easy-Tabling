f68139226e2477027eb71f02b60fbe8869083525
Toming <47220972+HKU3556759@users.noreply.github.com>
2019-04-27 17:02:39 +0800

    Update TableService.h

--- TableService.h
+++ TableService.h
@@ -7,7 +7,14 @@
 using namespace std;
 
 //TODO Add order
-
+void ordering(table*head, int table_no, int amount){
+  table *current = head;
+  while (current != NULL) {
+    if (current -> table_no == table_no){
+      current -> amount += amount;
+      return;}
+current = current->next;}
+cout<< "Table does not exist"<<endl;}
 //TODO notify when occupy too long
 
 //extern vector<Table> tables;
