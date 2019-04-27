f0931dde97627f687df79cc0ed975b837c23e705
Yau Herman <taikongrenqiu@hotmail.com>
2019-04-27 21:49:05 +0800

    Added comments for main.cpp

--- main.cpp
+++ main.cpp
@@ -10,34 +10,27 @@
 #include "TableService.h"
 using namespace std;
 
-void printTableStatus(table *t) {
-
-  cout << left;
-  cout << setw(10) << "Table No." << setw(10) << "Capacity" << setw(10) << "Status" << setw(10) << "Amount" << setw(15) << "Starting Time" << endl;
-
-  while (t != NULL) {
-    time_t time = chrono::system_clock::to_time_t(t->time);
-    cout << setw(10) << t->table_no << setw(10) << t->no_of_cus << setw(10) << t->status << setw(10) << t->amount << setw(15) << ctime(&time);
-    t = t->next;
-  }
-
-}
-
+//Main Program, has 3 parts.
+//1. Read input from a text file.
+//2. Handle commands from users while providing a text menu.
+//3. Output a report to a text file.
 int main() {
 
+  //Initialize linked list and default idle value
   table * head = NULL;
   table * tail = NULL;
   int idleMins = 120;
 
+  //Read input config file, terminate program if failed.
   ifstream fin;
   fin.open("config.txt");
-
   if (fin.fail()) {
-    cout << "Failed to read the configuration file!" << endl;
-    cout << "The program will end now..." << endl;
+    cout << "[!] Failed to read the configuration file!" << endl;
+    cout << "[!] The program will end now..." << endl;
     exit(1);
   }
 
+  //Create tables according to the config.
   string tmp;
   int n = 1, x;
   getline(fin,tmp);
@@ -47,29 +40,39 @@ int main() {
   }
   fin.close();
 
+  //The main menu
   char in;
   while (true) {
 
+    //Output all status of the tables and check idle time
     cout << endl;
-    printTableStatus(head);
+    printAllTableStatus(head);
     checkIdleTable(head, idleMins);
 
+    //Main menu layer 1
     cout << endl << "MAIN MENU" << endl;
     cout << "-----" << endl;
     cout << "1. Table Operation" << endl;
     cout << "2. Table Searching" << endl;
     cout << "3. Table Service" << endl;
     cout << "0. End Program" << endl << endl;
-    cout << "Please input number (0 - 3): > ";
+    cout << "[?] Please input number (0 - 3): > ";
 
-    cin >> x;
-    if (x == 0) {
-      cout << endl << "Program will end, are you sure? (Y/N): > ";
+    cin >> in;
+    if (in == '0') {
+
+      //Confirm program termination
+      cout << endl << "[?] Program will end, are you sure? (Y/N): > ";
       cin >> in;
-      if (in == 'Y') break;
-      else continue;
-    } else if (x == 1) {
+      if (in == 'Y') {
+        cout << endl << "[i] Thank you for using Easy Tabling." << endl;
+        cout << endl << "[i] Made by To Ming Lok and Yau Herman, all rights reserved." << endl;
+        cout << endl << "A report.txt will be generated at the root folder." << endl;
+        break;
+      }
+    } else if (in == '1') {
 
+      //Main menu layer 2
       cout << endl << "TABLE OPERATION" << endl;
       cout << "-----" << endl;
       cout << "+. Occupy table" << endl;
@@ -77,51 +80,64 @@ int main() {
       cout << "-. Release table" << endl;
       cout << "/. Change table configuration" << endl;
       cout << "0. Back" << endl << endl;
-      cout << "Please input symbol: > ";
+      cout << "[?] Please input symbol: > ";
 
       cin >> in;
       if (in == '+') {
 
-        cout << endl << "What table number to occupy? > ";
+        //Occupy a table
+        cout << endl << "[?] What table number to occupy? > ";
         cin >> x;
-        occupy_table(head, x);
+        if (occupy_table(head, x)) {
+          cout << "[i] Successfully occupied table " << x << "." << endl;
+        }
 
       } else if (in == '*') {
 
-        cout << endl << "What table number to reserve? > ";
+        //Reserve a table
+        cout << endl << "[?] What table number to reserve? > ";
         cin >> x;
-        reserve_table(head, x);
+        if (reserve_table(head, x)) {
+          cout << "[!] Successfully reserved table " << x << "." << endl;
+        }
 
       } else if (in == '-') {
 
-        cout << endl << "What table number to release? > ";
+        //Release a table
+        cout << endl << "[?] What table number to release? > ";
         cin >> x;
         int amount = release_table(head, x);
-        cout << "[i] Table " << x << " has been released, the total amount is $" << amount << "." << endl;
+        if (amount >= 0)
+          cout << "[i] Table " << x << " has been released, the total amount is $" << amount << "." << endl;
 
       } else if (in == '/') {
 
-        cout << endl << "What table number to change configuration? > ";
+        //Change capacity of a TableService
+        cout << endl << "[?] What table number to change configuration? > ";
         cin >> x;
-        cout << endl << "Change to what capacity? > ";
+        cout << endl << "[?] Change to what capacity? > ";
         cin >> n;
-        changeTable(head, x, n);
+        if (n <= 0) {
+          cout << endl << "[!] The new capacity needs to be larger than 0!" << endl;
+        } else changeTable(head, x, n);
 
       } else continue;
 
-    } else if (x == 2) {
+    } else if (in == '2') {
 
+      //Main menu layer 2
       cout << endl << "TABLE SEARCHING" << endl;
       cout << "-----" << endl;
       cout << "!. Get next avaliable table" << endl;
       cout << "?. Check status of a table" << endl;
       cout << "0. Back" << endl << endl;
-      cout << "Please input symbol: > ";
+      cout << "[?] Please input symbol: > ";
 
       cin >> in;
       if (in == '!') {
 
-        cout << endl << "How many customers? > ";
+        //Search for the next avaliable table
+        cout << endl << "[?] How many customers? > ";
         cin >> x;
 
         if (x <= 0) {
@@ -130,70 +146,80 @@ int main() {
 
         int tableNum = search_for_avaliable(head, x);
         if (tableNum <= 0) {
-          cout << endl << "[i] No avaliable for " << x << " customers right now." << endl;
+          cout << endl << "[i] No table avaliable for " << x << " customers right now." << endl;
         } else {
           cout << endl << "[i] Table " << tableNum << " is avaliable for " << x << " customers." << endl;
         }
 
       } else if (in == '?') {
 
-        cout << endl << "Which table to search for? > ";
+        //Check status of a table
+        cout << endl << "[?] Which table to search for? > ";
         cin >> x;
         check_table_status(x, head);
 
       } else continue;
 
-    } else if (x == 3) {
+    } else if (in == '3') {
 
+      //Main menu layer 2
       cout << endl << "TABLE SERVICE" << endl;
       cout << "-----" << endl;
-      cout << "+. Add order amount" << endl;
+      cout << "+. Add an order" << endl;
       cout << "@. Change table idle notification time" << endl;
       cout << "0. Back" << endl << endl;
-      cout << "Please input symbol: > ";
+      cout << "[?] Please input symbol: > ";
 
       cin >> in;
       if (in == '+') {
 
-        cout << endl << "Which table is ordering? > ";
+        //Add a order for a table
+        cout << endl << "[?] Which table is ordering? > ";
         cin >> x;
-        cout << endl << "How much is the amount of the order? ($) > ";
+        cout << endl << "[?] How much is the amount of the order? ($) > ";
         cin >> n;
-        ordering(head, x, n);
+        if (ordering(head, x, n))
         cout << endl << "[i] $" << n << " has been added for table " << x << ".";
 
       } else if (in == '@') {
 
+        //Change the idle value
         cout << endl << "[i] Current idle minutes is " << idleMins << "." << endl;
-        cout << "How many minutes should a occupied table considered idle? > ";
+        cout << "[?] How many minutes should a occupied table considered idle? > ";
         cin >> x;
+
         if (x <= 0) {
-          cout << "[!] Idle minutes time has to be larger than 0!" << endl;
+          cout << endl << "[!] Idle minutes time has to be larger than 0!" << endl;
         }
         idleMins = x;
         cout << endl << "[i] The idle time has been set to " << x << " minutes." << endl;
 
-      } else continue;
+      }
 
-    } else continue;
+    }
 
   }
 
+  //Output to a file, terminate if failed
   ofstream fout;
   fout.open("report.txt");
   if (fout.fail()) {
-    cout << "Failed to generate the report!" << endl;
-    cout << "The program will end now..." << endl;
+    cout << "[!] Failed to generate the report!" << endl;
+    cout << "[!] The program will end now..." << endl;
     exit(1);
   }
 
+  //Output with style... I mean format.
   fout << left;
   fout << setw(10) << "Table No." << setw(10) << "Capacity" << setw(10) << "Total ($)" << endl;
   table *current = head;
+  int sum = 0;
   while (current != NULL) {
     fout << setw(10) << current->table_no << setw(10) << current->no_of_cus << setw(10) << current->total_amount << endl;
+    sum += current->total_amount;
     table * tmp = current;
     current = current->next;
+    //Save some memory
     delete tmp;
   }
   fout.close();
