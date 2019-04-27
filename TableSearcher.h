ffcf0d6875e6854d6f445ce6e876492312d38adb
Toming <47220972+HKU3556759@users.noreply.github.com>
2019-04-27 16:41:21 +0800

    done

--- TableSearcher.h
+++ TableSearcher.h
@@ -4,12 +4,33 @@
 #include "Table.h"
 #include "TableManager.h"
 #include <vector>
+#include <iomanip>
 using namespace std;
 
-//TODO Search for the next avaliable table
+//TODO Search for the avaliable table
+int search_for_table(table *head,char status){
+    // read a status, this function returns the first  tables with given status
+    table *current = head;
+    while (current != NULL) {
+      // Idk is this while loop legal lol
+      if (current ->status == status){
+        return current -> table_no;
+     
+        }
+      current = current->next;}
+    return 0;}
+
 
 //TODO Check the status of a table
-
-//extern vector<Table> tables;
+void check_table_status(int table_no, table *head){
+  table *current = head;
+  while (current != NULL) {
+    // Idk is this while loop legal lol
+    if (current -> table_no == table_no){
+   cout << setw(10) << "Table No." << setw(10) << "Capacity" << setw(10) << "Status" << setw(10) << "Amount" << endl;
+   cout <<  setw(10) << current->table_no <<  setw(10) <<current->no_of_cus <<  setw(10) << current->status  <<  setw(10) <<current->amount<<endl;
+      }
+    current = current->next;}
+  return ;}
 
 #endif
